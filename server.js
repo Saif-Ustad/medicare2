f4966bb49cfa873c9c0adde0ec7c743785905098
Saif-Ustad <121380262+Saif-Ustad@users.noreply.github.com>
2023-04-16 15:31:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://medicare4u.onrender.com/auth/google/secrets",
+    callbackURL: "http://medicare4u.live/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
